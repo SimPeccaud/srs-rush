4537118f8903a0084fa67a3117a899a104a2d676
Christophe <christophe@christophe.ubuntu-domain>
2012-11-10 14:38:02 +0100

    - Modified whoami.php

--- www/whoami.php
+++ www/whoami.php
@@ -1,11 +1,17 @@
+<html>
+
+<head><title>Web Server identification</title></head>
+
+<body>
 
 <?php
-echo gethostname(); // may output e.g,: sandie
-
-// Or, an option that also works before PHP 5.3
-echo php_uname('n'); // may output e.g,: sandie
+echo gethostname();
 ?>
-
+<br>
+<br>
 <a href="admin.php">admin</a>
 <a href="catalog.php">catalog</a>
-<a href="whoami.php">admin</a>
+<a href="whoami.php">whoami</a>
+</body>
+
+</html>
