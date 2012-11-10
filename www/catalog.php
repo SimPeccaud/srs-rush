ea0c74cf64b338b858bef9a28e619b6c05b3dfbb
zed <zed@www-1.(none)>
2012-11-11 06:23:35 +1100

    updated nginx conf

--- www/catalog.php
+++ www/catalog.php
@@ -1,9 +1,20 @@
+<html>
+
+<head><title>Web Server catalog</title></head>
+
+<body>
+
+<a href="admin.php">admin</a>
+<a href="catalog.php">catalog</a>
+<a href="whoami.php">whoami</a>
+<br>
+<br>
 <?php
-ini_set('display_errors', 'On');
+try {
+
 error_reporting(E_ALL | E_STRICT);
 
-
-$connection = mysql_connect("localhost","store_ro","");
+$connection = mysql_connect("10.250.14.130","store_ro","");
 if (!$connection)
   {
    die ("Could not connect <b>".mysql_error());  
@@ -11,31 +22,25 @@ if (!$connection)
 
 mysql_select_db("store", $connection);
 
-$result = mysql_query("SELECT * FROM products");
+$result = mysql_query("SELECT *
+	  	FROM products
+		INNER JOIN stocks ON products.product_id = stocks.product_id");
 
 if (!$result)
 {
  die ("Could not connect <b>".mysql_error());  
 }
 
-  echo "name &nbsp;&nbsp;&nbsp;&nbsp;price &nbsp;&nbsp;&nbsp;&nbsp;quantity";
-  echo "<br />";
-while($row = mysql_fetch_array($result))
-  {
-    
-    $id = $row['product_id'] ;
-    $quantity = mysql_query("SELECT quantity FROM stocks WHERE product_id = $id");
-    $rowQ = mysql_fetch_array($quantity);
-    if (!$quantity)
-    {
-	 die ("Could not connect <b>".mysql_error());  
-    }
-    echo $row['name'] ."&nbsp;&nbsp;". $row['price'];
-    echo "&nbsp;&nbsp;";
-    echo $rowQ['quantity'];
-    echo "<br />";
-  }
+echo "<table border=2>
+     <tr><td>Name</td><td>Price</td><td>Quantity</td><td>Warehouse</td></tr>";
 
+while ($row = mysql_fetch_array($result))
+    printf("<tr><td>%s</td><td>%d</td><td>%d</td><td>%s</td></tr>",
+		$row['name'], $row['price'], $row['quantity'], $row['warehouse']);
+
+echo "</table>";
 mysql_close($connection);
-?> 
 
+} catch (Exception $e) {
+}
+?> 
--- www/whoami.php
+++ www/whoami.php
@@ -4,14 +4,16 @@
 
 <body>
 
-<?php
-echo gethostname();
-?>
-<br>
-<br>
 <a href="admin.php">admin</a>
 <a href="catalog.php">catalog</a>
 <a href="whoami.php">whoami</a>
+<br>
+<br>
+
+<?php
+echo gethostname();
+?>
+
 </body>
 
 </html>
