4c4773c3f0537ea037c86dfdaf892da4edebdd69
root <root@MinhPC.(none)>
2012-11-10 16:06:26 +0100

    modifie catalog

--- www/catalog.php
+++ www/catalog.php
@@ -1,4 +1,8 @@
 <?php
+ini_set('display_errors', 'On');
+error_reporting(E_ALL | E_STRICT);
+
+
 $connection = mysql_connect("localhost","store_ro","");
 if (!$connection)
   {
@@ -11,14 +15,27 @@ $result = mysql_query("SELECT * FROM products");
 
 if (!$result)
 {
-
+ die ("Could not connect <b>".mysql_error());  
 }
+
+  echo "name &nbsp;&nbsp;&nbsp;&nbsp;price &nbsp;&nbsp;&nbsp;&nbsp;quantity";
+  echo "<br />";
 while($row = mysql_fetch_array($result))
   {
-  echo $row['name'] . " " . $row['price'];
-  echo "<br />";
+    
+    $id = $row['product_id'] ;
+    $quantity = mysql_query("SELECT quantity FROM stocks WHERE product_id = $id");
+    $rowQ = mysql_fetch_array($quantity);
+    if (!$quantity)
+    {
+	 die ("Could not connect <b>".mysql_error());  
+    }
+    echo $row['name'] ."&nbsp;&nbsp;". $row['price'];
+    echo "&nbsp;&nbsp;";
+    echo $rowQ['quantity'];
+    echo "<br />";
   }
 
-mysql_close($con);
+mysql_close($connection);
 ?> 
 
