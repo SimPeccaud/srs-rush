bf0b507ca0e603ac29d4acb8ea8a0ba5e506b686
Christophe <christophe@christophe.ubuntu-domain>
2012-11-10 12:27:24 +0100

    Modified whoami.php

---
+++ www/whoami.php
@@ -0,0 +1,11 @@
+
+<?php
+echo gethostname(); // may output e.g,: sandie
+
+// Or, an option that also works before PHP 5.3
+echo php_uname('n'); // may output e.g,: sandie
+?>
+
+<a href="admin.php">admin</a>
+<a href="catalog.php">catalog</a>
+<a href="whoami.php">admin</a>
